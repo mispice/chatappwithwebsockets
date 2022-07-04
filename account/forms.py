93f27044144555081a947734c6ef357207ae3217
mispice <minaseberhane@gmail.com>
2022-07-04 11:10:47 -0700

    registration functionality

---
+++ account/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+
+from account.models import Account
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=255, help_text="Required. Add a valid email addresss.")
+
+    class Meta:
+        model = Account
+        fields = ('email','username','password1','password2')
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        try:
+            account = Account.objects.get(email = email)
+        except Exception as e:
+            raise
+        raise forms.ValidationError(f"Email {email} is already in use.")
+
+    def clean_username(self):
+        username= self.cleaned_data['username']
+        try:
+            account = Account.objects.get(username = username)
+        except Exception as e:
+            raise
+        raise forms.ValidationError(f"Username {username} is already in use.")
